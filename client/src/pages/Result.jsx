4be9bee5b1b59c26f51aa26780438b1cc0e73e2e
kanto-taniguchi <kanto.jobs@gmail.com>
2025-03-29 20:34:45 +0900

    add: Result Page

---
+++ client/src/pages/Result.jsx
@@ -0,0 +1,12 @@
+// client/src/pages/Result.jsx
+import React from "react";
+
+function Result() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl">Scan Results</h1>
+    </div>
+  );
+}
+
+export default Result;
